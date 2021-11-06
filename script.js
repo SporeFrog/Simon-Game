35f207b036808870a34494aaa98545c257e9a925
Aaron <anaugustchill@gmail.com>
2021-11-06 18:45:33 -0400

    Added Sounds and now specific sound will play

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var gamePattern = [];
 
+const blueSound = new Audio('/sounds/blue.mp3');
+const greenSound = new Audio('/sounds/green.mp3');
+const redSound = new Audio('/sounds/red.mp3');
+const yellowSound = new Audio('/sounds/yellow.mp3');
+
 var buttonColors = ["red", "blue", "green", "yellow"];
 function nextSequence(){
 var randomNumber = Math.floor(Math.random()*4);
 var randomColor = buttonColors[randomNumber];
 gamePattern.push(randomColor);
 var chosenButton = $(String("."+randomColor));
-chosenButton.fadeOut(200).fadeIn(200);
+var chosenSound = "/sounds/" + randomColor + ".mp3";
+console.log(chosenSound);
+var currentSound = new Audio(String(chosenSound));
+currentSound.play();
+chosenButton.fadeOut(150).fadeIn(150);
 }
 
