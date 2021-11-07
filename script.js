f5333077ca5b6547de0e19df56b8b68405576d14
Aaron <anaugustchill@gmail.com>
2021-11-07 17:48:13 -0500

    Updating Levels and adding start with keypress

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+
+
+
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 function nextSequence(){
@@ -12,14 +16,22 @@ console.log(chosenSound);
 var currentSound = new Audio(String(chosenSound));
 currentSound.play();
 chosenButton.fadeOut(150).fadeIn(150);
+$('h1').text("Level " + level);
+
+level++;
 }
 
 $('.button').on("click", function(){
+
 var userChosenColor = $(this).attr('id');
 userClickedPattern.push(userChosenColor)
 console.log(userClickedPattern);
 var clickedSound = "/sounds/" + userChosenColor + ".mp3";
 var specificSound = new Audio(String(clickedSound));
 specificSound.play();
+$(this).fadeOut(150).fadeIn(150);
 });
+
+$(document).on("keypress", nextSequence);
+
 
